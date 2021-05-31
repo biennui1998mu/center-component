95b7b476bbbbd30d085cf32d511c42d3b73c7e01
Nguyen Son <sonndh@meeyland.com>
2021-05-31 17:53:45 +0700

    fix

--- center-component/src/components/center.vue
+++ center-component/src/components/center.vue
@@ -54,14 +54,14 @@ import moment from 'moment'
           ranges: {
             'Today': [moment(), moment()],
             'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'Last week': [moment().subtract(6, 'days'), moment()],
             'This Month': [moment().startOf('month'), moment().endOf('month')],
-            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+            'All Time': []
           },
           showCustomRangeLabel: true,
           alwaysShowCalendars: true,
-          linkedCalendars: false
+          linkedCalendars: false,
         },
         format: 'MMMM DD, YYYY',
         items: [
