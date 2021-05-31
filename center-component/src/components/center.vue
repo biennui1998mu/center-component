8c675d897575a672663ced20059e8af5a505b435
Quỳnh <quynhnv@meeyland.com>
2021-05-31 16:37:53 +0700

    Quynh up

--- center-component/src/components/center.vue
+++ center-component/src/components/center.vue
@@ -1,21 +1,69 @@
 <template>
-  <div>
-    <b-button>Button</b-button>
-    <b-button variant="danger">Button</b-button>
-    <b-button variant="success">Button</b-button>
-    <b-button variant="outline-primary">Button</b-button>
+  <div class="container">
+    <h2 class="page-title">
+      Subscriptions
+    </h2>
+    <b-row class="mt-3">
+      <b-col class="text-center bg-light">
+        <p>Subscriptions</p>
+        <h4>156</h4>
+      </b-col>
+      <b-col class="text-center bg-light">
+        <p>MRR</p>
+        <h4>$2,861</h4>
+      </b-col>
+      <b-col class="text-center bg-light">
+        <p>Yearly revenue</p>
+        <h4>$34,333</h4>
+      </b-col>
+    </b-row>
+    <div class="custom-table">
+      <div class="row">
+        <div class="col-md-6">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <div class="col-md-3">
+          <select id="inputState" class="form-control">
+            <option selected>Choose...</option>
+            <option>1</option>
+            <option>1</option>
+            <option>1</option>
+          </select>
+        </div>
+        <div class="col-md-3">
+          <date-range-picker v-model="range" class="form-control"/>
+        </div>
+      </div>
+      <div class="row">
+        <b-table striped hover :items="items" :fields="fields"></b-table>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
+  export default {
+    name: 'HelloWorld',
+    data() {      
+      return {
+        // Note `isActive` is left out and will not appear in the rendered table
+        fields: ['first_name', 'last_name', 'age'],
+        items: [
+          { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
+        
+      }      
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-
+body{
+  background: #000;
+}
 </style>
--- center-component/src/main.js
+++ center-component/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// import the plugin
+import DateRangePicker from "@gravitano/vue-date-range-picker";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import '../node_modules/bootstrap/scss/bootstrap.scss';
@@ -10,6 +12,8 @@ import '../node_modules/bootstrap-vue/src/index';
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+// use the plugin
+Vue.use(DateRangePicker);
 
 Vue.config.productionTip = false
 
