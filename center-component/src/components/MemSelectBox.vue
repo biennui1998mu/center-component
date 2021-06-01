b90a94d5ba0608dca4b98453e42bb2af4ad4f914
Nguyen Son <sonndh@meeyland.com>
2021-06-01 13:35:36 +0700

    fix checkbox

--- center-component/src/components/MemSelectBox.vue
+++ center-component/src/components/MemSelectBox.vue
@@ -1,31 +1,41 @@
 <template>
   <div>
-    
-
-    <svg xmlns="http://www.w3.org/2000/svg" style="display: none">
-      <symbol id="checkmark" viewBox="0 0 12 10">        
-        <path d="M11.6795 0.827822L10.9024 0.214251C10.5193 -0.0877654 10.2967 -0.0842683 9.96874 0.313122L4.32044 7.1511L1.69186 5.00869C1.32922 4.70985 1.10237 4.72574 0.804223 5.09134L0.20436 5.85751C-0.0999462 6.23328 -0.0610573 6.4466 0.298666 6.74131L4.04497 9.78118C4.43062 10.0991 4.64775 10.0663 4.9459 9.71061L11.7771 1.74341C12.0979 1.36509 12.0785 1.13906 11.6795 0.827822Z" fill="#14C8B1"/>
-      </symbol>
-    </svg>
-    <div class="promoted-checkbox">
-      <input id="tmp" type="checkbox" class="promoted-input-checkbox"/>
-      <label for="tmp">
-        <svg><use xlink:href="#checkmark" /></svg>      
-        Select All Courses   
-      </label>
-    </div>
     <b-dropdown :text="selection.join()" menu-class="w-100" block variant="outline-primary"
               class="w-100">
     <input v-model="search" type="text" class="form-control" placeholder="Search">
     <b-dropdown-form class="mem-dropdown">
-      <b-form-checkbox v-model="selectAll" class="mb-1">Select All Courses</b-form-checkbox>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" style="display: none">
+          <symbol id="checkmark" viewBox="0 0 12 10">
+            <path d="M11.6795 0.827822L10.9024 0.214251C10.5193 -0.0877654 10.2967 -0.0842683 9.96874 0.313122L4.32044 7.1511L1.69186 5.00869C1.32922 4.70985 1.10237 4.72574 0.804223 5.09134L0.20436 5.85751C-0.0999462 6.23328 -0.0610573 6.4466 0.298666 6.74131L4.04497 9.78118C4.43062 10.0991 4.64775 10.0663 4.9459 9.71061L11.7771 1.74341C12.0979 1.36509 12.0785 1.13906 11.6795 0.827822Z" fill="#14C8B1"/>
+          </symbol>
+        </svg>
+        <div class="promoted-checkbox">
+          <input id="selectAll" v-model="selectAll" type="checkbox" class="promoted-input-checkbox"/>
+          <label for="selectAll">
+            <svg><use xlink:href="#checkmark" /></svg>
+            Select All Courses
+          </label>
+        </div>
+      </div>
       <b-form-checkbox-group v-model="selection" v-for="(item, index) in filterArray" :key="index">
         <b-dropdown-header class="header-custom">
           {{ item.label }}
         </b-dropdown-header>
-        <b-form-checkbox v-for="(option, index) in item.value" :key="index" :value="option.value" class="mb-1">
-          {{ option.label }}
-        </b-form-checkbox>
+        <div v-for="(option, index) in item.value" :key="index">
+          <svg xmlns="http://www.w3.org/2000/svg" style="display: none">
+            <symbol id="checkmark" viewBox="0 0 12 10">
+              <path d="M11.6795 0.827822L10.9024 0.214251C10.5193 -0.0877654 10.2967 -0.0842683 9.96874 0.313122L4.32044 7.1511L1.69186 5.00869C1.32922 4.70985 1.10237 4.72574 0.804223 5.09134L0.20436 5.85751C-0.0999462 6.23328 -0.0610573 6.4466 0.298666 6.74131L4.04497 9.78118C4.43062 10.0991 4.64775 10.0663 4.9459 9.71061L11.7771 1.74341C12.0979 1.36509 12.0785 1.13906 11.6795 0.827822Z" fill="#14C8B1"/>
+            </symbol>
+          </svg>
+          <div class="promoted-checkbox">
+            <input :id="option.label" v-model="selection" :value="option.value" type="checkbox" class="promoted-input-checkbox"/>
+            <label :for="option.label">
+              <svg><use xlink:href="#checkmark" /></svg>
+              {{ option.label.charAt(0).toUpperCase() + option.label.slice(1)}}
+            </label>
+          </div>
+        </div>
       </b-form-checkbox-group>
     </b-dropdown-form>
   </b-dropdown>
